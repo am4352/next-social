b713f31c1d3f2d3925f73f4c833f34cef680abf7
am4352 <am4352@srmist.edu.in>
2025-04-13 23:14:06 +0530

    hoempage updated

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,28 +1,90 @@
-import Image from "next/image"
-
+import Image from "next/image";
+import Comments from "./Comments";
 const Post = () => {
   return (
-    <div className='flex flex-col gap-4'>
-        {/*USER*/}
-        <div className=''>
-            <div className='flex item-center gap-4'>
-                <Image src="/cat.png" width={40} height={40} alt="" className=' w-10 h-10 rounded-full' />
-            <span className="font-medium">Shashwat Mishra</span>    
-            </div>
+    <div className="flex flex-col gap-4 p-4 border rounded-lg shadow-sm bg-white">
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="/cat.png"
+            width={40}
+            height={40}
+            alt="User Avatar"
+            className="w-10 h-10 rounded-full"
+          />
+          <span className="font-medium">Shashwat Mishra</span>
         </div>
-        {/*DESC*/}
-        <div className=''></div>
-        {/*INTERACTION*/}
-        <div className=''></div>
+      </div>
+
+      {/* DESC */}
+      <div className="flex flex-col gap-4">
+        <div className="w-full min-h-96 relative">
+          <Image
+            src="/river.png"
+            alt="Post Image"
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsam nihil
+          sunt modi consequatur, ab perspiciatis aspernatur enim at porro.
+        </p>
+      </div>
+
+      {/* INTERACTIONS */}
+      <div className="flex items-center justify-between text-sm my-4">
+        <div className="flex gap-8">
+          <div className="flex items-center gap-2 bg-slate-100 p-2 rounded-xl">
+            <Image
+              src="/like.png"
+              width={20}
+              height={20}
+              className="cursor-pointer"
+              alt="Like"
+            />
+            <span className="text-gray-300">1</span>
+            <span className="text-gray-500">123
+              <span className="hidden md:inline"> Likes</span>
+            </span>
+          </div>
+          <div className="flex items-center gap-2 bg-slate-100 p-2 rounded-xl">
+            <Image
+              src="/comment.png"
+              width={20}
+              height={20}
+              className="cursor-pointer"
+              alt="Like"
+            />
+            <span className="text-gray-300">1</span>
+            <span className="text-gray-500">123
+              <span className="hidden md:inline"> comments</span>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex items-center gap-2 bg-slate-100 p-2 rounded-xl">
+            <Image
+              src="/share.png"
+              width={20}
+              height={20}
+              className="cursor-pointer"
+              alt="Like"
+            />
+            <span className="text-gray-300">1</span>
+            <span className="text-gray-500">123
+              <span className="hidden md:inline"> Share</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Comments/>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Post
-
-
-
+export default Post;
 
 
 
