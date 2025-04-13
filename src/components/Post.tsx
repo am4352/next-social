6b87295025f39e99ce6e3148a59bc95eac6d0307
am4352 <am4352@srmist.edu.in>
2025-04-13 22:07:51 +0530

    psot added

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,8 +1,14 @@
-
+import Image from "next/image"
 const Post = () => {
   return (
     <div className='flex flex-col gap-4'>
-        {/*USER*/}
+      {/*USER*/}
+      <div className="flex items-center gap-4">
+        <Image src="" alt="" className="w-10 h-10 rounded-full"/>
+      </div>
+
+
+
     </div>
   )
 }
