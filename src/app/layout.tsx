3b06a788d18986b69003c12ea665d8d53dc3bda5
am4352 <am4352@srmist.edu.in>
2025-04-13 21:48:48 +0530

    test-merge branch created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import {
 } from '@clerk/nextjs'
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/NavBar";
+import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
