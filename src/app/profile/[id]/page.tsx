f9b4f9abb7404083e53006601c384c0842982557
shashwat <shashwatmishra15082004@gmail.com>
2025-04-14 22:07:15 +0530

    created  usermedia, userinfo

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,22 @@
+import Feed from "@/components/Feed"
+import LeftMenu from "@/components/leftMenu/LeftMenu"
+import RightMenu from "@/components/rightMenu/RightMenu"
 const profilepage = () => {
     return (
-        <div className=''>profilepage</div>
+        <div className="flex gap-6 pt-6">
+        <div className="hidden xl:block w-[20%]">
+          <LeftMenu type="home" />
+        </div>
+        <div className="w-full lg:w-[70%] xl:w-[50%]">
+          <div className="flex flex-col gap-6">
+            
+            <Feed />
+          </div>
+        </div>
+        <div className="hidden lg:block w-[30%]">
+          <RightMenu userId="test"/>
+        </div>
+      </div>  
     )
 }
 
---
+++ src/components/UserMediaCard.tsx
@@ -0,0 +1,37 @@
+import Feed from "./Feed"
+import LeftMenu from "./leftMenu/LeftMenu"
+import RightMenu from "./rightMenu/RightMenu"
+import Link from "next/link"
+import Image from "next/image"
+
+const UserMediaCard = ({userId}:{userId:string}) => {
+  return (
+    
+    <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
+        {/*TOP*/}
+        <div className="flex justify-between items-center font-medium">
+            <span className="text-gray-500">User Media</span>
+            <Link href="/" className="text-blue-500 text-xs">See all</Link>
+        </div>
+        {/*BOTTOM*/}
+        <div className="flex flex-col gap-4 text-gray-500">
+            <div className="flex items-center gap-2">
+              <span className="text-xl text-black">Shashwat Mishra</span>
+              <span className="text-sm">@soul</span>
+            </div>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt excepturi, distinctio cum fugiat hic nobis officia
+            </p>    
+                <div className="flex items-center gap-2">
+                    <Image src="/map.png" alt="" width={16} height={16}/>
+                    <span>Living in <b>Denver</b></span>
+                </div>
+            
+        </div>
+        </div>
+     
+    
+  )
+}
+
+export default UserMediaCard
