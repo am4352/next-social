45cd0cdd182674f1fe9be068836096e7d820a106
am4352 <am4352@srmist.edu.in>
2025-04-15 11:39:43 +0530

    completed till 2:2

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,23 +1,50 @@
-import Feed from "@/components/Feed"
-import LeftMenu from "@/components/leftMenu/LeftMenu"
-import RightMenu from "@/components/rightMenu/RightMenu"
+import Feed from "@/components/Feed";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
+import RightMenu from "@/components/rightMenu/RightMenu";
+import Image from "next/image";
 const profilepage = () => {
-    return (
-        <div className="flex gap-6 pt-6">
-        <div className="hidden xl:block w-[20%]">
-          <LeftMenu type="profile" />
-        </div>
-        <div className="w-full lg:w-[70%] xl:w-[50%]">
-          <div className="flex flex-col gap-6">
-            
-            <Feed />
+  return (
+    <div className="flex gap-6 pt-6">
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu type="profile" />
+      </div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image src="/tree.png" alt="" fill className="object-cover " />
+              <Image
+                src="/tree.png"
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto object-cover -bottom-16 ring-4 ring-white"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Anuj Mishra</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">123</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">1.2K</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">1.3K</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
           </div>
+          <Feed />
         </div>
-        <div className="hidden lg:block w-[30%]">
-          <RightMenu userId="test"/>
-        </div>
-      </div>  
-    )
-}
+      </div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu userId="test" />
+      </div>
+    </div>
+  );
+};
 
-export default profilepage
+export default profilepage;
