3ef0461ffbd3526ab1d110ad4cf9865532f05d9b
am4352 <am4352@srmist.edu.in>
2025-04-23 16:28:26 +0530

    completed the user info part

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,6 +1,7 @@
 "use server"
 import { auth } from "@clerk/nextjs/server";
 import prisma from "./client";
+import { z } from "zod";
 export const switchFollow = async (userId: string) => {
     const { userId: currentUserId } = await auth();
 
@@ -151,6 +152,53 @@ export const declineFollowRequest = async (userId: string) => {
         throw new Error("Something went wrong!");
     }
 }
+
+export const updateProfile = async (
+    prevStage: { success: boolean; error: boolean },
+    payload: { formData: FormData; cover: string }): Promise<{ success: boolean; error: boolean }> => {
+    const { formData, cover } = payload;
+   // const { success, error } = prevStage;
+    const fields = Object.fromEntries(formData);
+    const filteredFields = Object.fromEntries(
+        Object.entries(fields).filter(([_, value]) => value !== "")
+    );
+
+
+    const Profile = z.object({
+        cover: z.string().optional(),
+        name: z.string().max(60).optional(),
+        surname: z.string().max(60).optional(),
+        description: z.string().max(255).optional(),
+        city: z.string().max(60).optional(),
+        school: z.string().max(60).optional(),
+        work: z.string().max(60).optional(),
+        website: z.string().max(60).optional(),
+    });
+    const validatedFields = Profile.safeParse({cover,...filteredFields});
+
+    if (!validatedFields.success) {
+        console.log(validatedFields.error.flatten().fieldErrors);
+        return {success: false, error: true };
+    }
+    const { userId } = await auth();
+
+    if (!userId) {
+        return { success: false, error: true };
+    }
+
+    try {
+        await prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: validatedFields.data,
+        });
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err);
+        return { success: false, error: true }; 
+    }
+};
 
 
 
