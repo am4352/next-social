cdaf8d07ad065ec631eeb509e275a2b4d5a10fe4
am4352 <am4352@srmist.edu.in>
2025-04-27 16:26:34 +0530

    added delete and story feature

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,28 +1,43 @@
 
-const Stories = () => {
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import StoryList from "./StoryList";
+
+
+const Stories = async () => {
+  const { userId: currentUserId } = await auth();
+
+  if (!currentUserId) return null;
+
+  const stories = await prisma.story.findMany({
+    where: {
+      expiresAt: {
+        gt: new Date(),
+      },
+      OR: [
+        {
+          user: {
+            followers: {
+              some: {
+                followerId: currentUserId,
+              },
+            },
+          },
+        },
+        {
+          userId: currentUserId,
+        },
+      ],
+    },
+    include: {
+      user: true,
+    },
+  });
     return (
         <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
             <div className="flex gap-8 w-max">
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPG9V0UikyUN5AbB0SR2-vY26SQLgCye22laXr_B1vxLjbdIgFeCUHi1VLtoEtAFuvuJ0HB5HP4bjwaWe2hBFRhoen8FPHommpBvKBVEw__A" alt="" width={80} className="w-20 h-20 rounded-full ring-2" />
-              <span className="font-medium">ricky</span>                        
-               </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPG9V0UikyUN5AbB0SR2-vY26SQLgCye22laXr_B1vxLjbdIgFeCUHi1VLtoEtAFuvuJ0HB5HP4bjwaWe2hBFRhoen8FPHommpBvKBVEw__A" alt="" width={80} className="w-20 h-20 rounded-full ring-2" />
-              <span className="font-medium">ricky</span>                        
-               </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPG9V0UikyUN5AbB0SR2-vY26SQLgCye22laXr_B1vxLjbdIgFeCUHi1VLtoEtAFuvuJ0HB5HP4bjwaWe2hBFRhoen8FPHommpBvKBVEw__A" alt="" width={80} className="w-20 h-20 rounded-full ring-2" />
-              <span className="font-medium">ricky</span>                        
-               </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPG9V0UikyUN5AbB0SR2-vY26SQLgCye22laXr_B1vxLjbdIgFeCUHi1VLtoEtAFuvuJ0HB5HP4bjwaWe2hBFRhoen8FPHommpBvKBVEw__A" alt="" width={80} className="w-20 h-20 rounded-full ring-2" />
-              <span className="font-medium">ricky</span>                        
-               </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPG9V0UikyUN5AbB0SR2-vY26SQLgCye22laXr_B1vxLjbdIgFeCUHi1VLtoEtAFuvuJ0HB5HP4bjwaWe2hBFRhoen8FPHommpBvKBVEw__A" alt="" width={80} className="w-20 h-20 rounded-full ring-2" />
-              <span className="font-medium">ricky</span>                        
-               </div>
+                <StoryList stories={stories} userId={currentUserId} />
             </div>
         </div>
     );
--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import Comments from "./Comments";
-import {Post as PostType, User} from "@prisma/client";
+import { Post as PostType, User } from "@prisma/client";
 import PostInteraction from "./PostInteraction";
+import { Suspense } from "react";
+import PostInfo from "./PostInfo";
+import { auth } from "@clerk/nextjs/server";
 
 type FeedPostType = PostType & { user: User } & {
   likes: [{ userId: string }];
 } & {
   _count: { comments: number };
+};
 
-} 
-
-const Post = ({post}:{post:FeedPostType}) => {
+const Post = async({ post }: { post: FeedPostType }) => {
+ const {userId} = await auth()
   return (
-    <div className="flex flex-col gap-4 p-4 border rounded-lg shadow-sm bg-white">
+    <div className="flex flex-col gap-4">
       {/* USER */}
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
@@ -20,18 +23,20 @@ const Post = ({post}:{post:FeedPostType}) => {
             src={post.user.avatar || "/noAvatar.png"}
             width={40}
             height={40}
-            alt="User Avatar"
+            alt=""
             className="w-10 h-10 rounded-full"
           />
-          <span className="font-medium">{
-          (post.user.name && post.user.surname) ? post.user.name + " " + post.user.surname : post.user.username}
+          <span className="font-medium">
+            {post.user.name && post.user.surname
+              ? post.user.name + " " + post.user.surname
+              : post.user.username}
           </span>
         </div>
+        {userId === post.user.id && <PostInfo postId={post.id} />}
       </div>
-
       {/* DESC */}
       <div className="flex flex-col gap-4">
-       {post.img && (
+        {post.img && (
           <div className="w-full min-h-96 relative">
             <Image
               src={post.img}
@@ -41,20 +46,21 @@ const Post = ({post}:{post:FeedPostType}) => {
             />
           </div>
         )}
-         <p>{post.desc}</p>
+        <p>{post.desc}</p>
       </div>
-
-      {/* INTERACTIONS */}
-      <PostInteraction postId={post.id} likes={post.likes.map((like)=>like.userId)} commentNumber={post._count.comments}/>
-      <Comments postId={post.id}/>
+      {/* INTERACTION */}
+      <Suspense fallback="Loading...">
+        <PostInteraction
+          postId={post.id}
+          likes={post.likes.map((like) => like.userId)}
+          commentNumber={post._count.comments}
+        />
+      </Suspense>
+      <Suspense fallback="Loading...">
+        <Comments postId={post.id} />
+      </Suspense>
     </div>
   );
 };
 
 export default Post;
-
-
-
-
-
-
